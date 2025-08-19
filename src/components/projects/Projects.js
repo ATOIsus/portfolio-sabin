a665d07e391aa87fba08d9a64f4e0ae6c29a05c9
atoisus <sabinmaharjan884@gmail.com>
2025-08-19 19:23:18 +1000

    feat: Testing Updates

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,6 +7,7 @@ const Projects = () => {
     <div className="projects-page">
       <main className="main-content">
         <h1>Will soon add them :)</h1>
+        <h2>Adding them real soon :)</h2>
       </main>
     </div>
   );
